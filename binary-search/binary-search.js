36edfecd5489acdda67de9dc0a199446f225a292
Ross Sicherman <ross.sicherman@gmail.com>
2019-01-22 19:01:47 -0500

    Completes algorithms implementation

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,19 @@
 'use strict';
 
 // Complete this algo
+
 const binarySearch = (array, target) => {
-	
+  const middleIndex = Math.floor(array.length / 2.0);
+
+  if (target === array[middleIndex]) {
+    return true;
+  } else if (array.length <= 1) {
+    return false;
+  } else if (target > array[middleIndex]) {
+    return binarySearch(array.slice(middleIndex + 1), target);
+  } else if (target < array[middleIndex]) {
+    return binarySearch(array.slice(0, middleIndex), target);
+  }
 };
 
 /*
@@ -14,4 +25,23 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+const binarySearch2 = (array, target, startIndex, endIndex) => {
+  startIndex = startIndex || 0;
+  endIndex = endIndex || array.length - 1;
+
+  const middleIndex = Math.floor((endIndex + startIndex) / 2.0);
+
+  console.log(middleIndex);
+
+  if (target === array[middleIndex]) {
+    return true;
+  } else if (startIndex > endIndex) {
+    return false;
+  } else if (target > array[middleIndex]) {
+    return binarySearch(array, target, middleIndex + 1, endIndex);
+  } else if (target < array[middleIndex]) {
+    return binarySearch(array, target, startIndex, middleIndex - 1);
+  }
+};
+
+module.exports = binarySearch;
